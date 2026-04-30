4b3c2203240fcc16888e215d054e3163a877116a
Özcan Elmacı <oz.ozcan.elmaci55@gmail.com>
2026-05-01 01:55:04 +0300

    Add Project Files

---
+++ FlightBooking/ViewComponents/DefaultViewComponents/_DefaultNavbarComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultNavbarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
