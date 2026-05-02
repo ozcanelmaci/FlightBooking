aa03245534dacccb88cfdf886620061d6fc42f49
Özcan Elmacı <oz.ozcan.elmaci55@gmail.com>
2026-05-02 20:00:04 +0300

    Ders Sonu 13

---
+++ FlightBooking/ViewComponents/BookingViewComponents/_BookingTagsComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.ViewComponents.BookingViewComponents
+{
+    public class _BookingTagsComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke(List<string> tags)
+        {
+            return View(tags);
+        }
+    }
+}
---
+++ FlightBooking/ViewComponents/DefaultViewComponents/_DefaultSliderComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultSliderComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
